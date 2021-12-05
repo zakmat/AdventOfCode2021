870be4abf49ecf834c8916916bc0d779d8d5d983
Mateusz Żak <mateusz.zak@gmail.com>
2021-12-05 08:28:05 +0100

    Day 5 solution
    * neat `withDefault` Map extension to simplify counting code (similar to defaultdict in Python)
    * `toward` helper function that disregards step direction

--- src/day5/Day5.kt
+++ src/day5/Day5.kt
@@ -4,21 +4,55 @@ import java.io.File
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    solve(File("src/day4/test.txt"))
-    solve(File("src/day4/input.txt"))
+    solve(File("src/day5/test.txt"))
+    solve(File("src/day5/input.txt"))
+}
+
+data class Segment(val startx: Int, val starty: Int, val endx: Int, val endy: Int)
+
+infix fun Int.toward(to: Int): IntProgression {
+    val step = if (this > to) -1 else 1
+    return IntProgression.fromClosedRange(this, to, step)
+}
+
+fun Segment.calculatePoints(withDiagonals: Boolean): List<Pair<Int, Int>> {
+    if (startx == endx) {
+        return (starty toward endy).map { startx to it }
+    }
+    if (starty == endy) {
+        return (startx toward endx).map { it to starty }
+    }
+
+    if (!withDiagonals) {
+        return emptyList()
+    }
+
+    return (startx toward endx).zip(starty toward endy)
 }
 
 fun solve(file: File) {
     println("---- ${file.nameWithoutExtension} ----")
-    val eol = System.lineSeparator()
-    val text = file.readText().trim().split("$eol$eol")
+    val segments = file.readLines().map { line ->
+        val start = line.substringBefore(" -> ").split(',').map(String::toInt).let { it.first() to it.last() }
+        val end = line.substringAfter(" -> ").split(',').map(String::toInt).let { it.first() to it.last() }
+        Segment(start.first, start.second, end.first, end.second)
+    }
 
     val timeInMillis = measureTimeMillis {
-
-        val stage1result = null
+        val stage1result = countOverlaps(segments, false)
         println("Stage1: $stage1result")
-        val stage2result = null
+        val stage2result = countOverlaps(segments, true)
         println("Stage2: $stage2result")
     }
     println("---- ${file.nameWithoutExtension} ---- Elapsed time: $timeInMillis")
+}
+
+fun countOverlaps(segments: List<Segment>, withDiagonals: Boolean): Int {
+    val points = mutableMapOf<Pair<Int, Int>, Int>().withDefault { 0 }
+    segments.forEach { segment ->
+        segment.calculatePoints(withDiagonals).forEach { point ->
+            points[point] = 1 + points.getValue(point)
+        }
+    }
+    return points.values.count { it > 1 }
 }
