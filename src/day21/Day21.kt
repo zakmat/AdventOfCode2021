6b47d5bbef7102fd5a9b788b092930e1077bd185
Mateusz Żak <mateusz.zak@gmail.com>
2021-12-21 21:34:42 +0100

    add regular DP solution and compare results with memoization

--- src/day21/Day21.kt
+++ src/day21/Day21.kt
@@ -1,9 +1,8 @@
 package day21
 
-import utils.Memoized
-import utils.plus
-import utils.times
+import utils.*
 import java.lang.Long.max
+import java.util.*
 import kotlin.system.measureTimeMillis
 
 data class Die(val max: Int, var current: Int)
@@ -42,16 +41,73 @@ fun solve(dataSet: String, start: Pair<Int, Int>) {
     val timeInMillis = measureTimeMillis {
         val stage1result = countDeterministic(start)
         println("Stage1: $stage1result")
+        val initial = State(
+            listOf(0, 0),
+            listOf(start.first, start.second),
+            0
+        )
 
-        val stage2resultCached = countWins(State(listOf(0, 0), listOf(start.first, start.second), 0)).let { max(it.first, it.second) }
+        val stage2resultCached = countWinsCached(
+            initial
+        ).let { max(it.first, it.second) }
         println("Stage2: $stage2resultCached")
+
+
+        val stage2resultDp = countWinsDp(initial)
+        println("Stage2: $stage2resultDp")
     }
     println("---- $dataSet ---- Elapsed time: $timeInMillis")
 }
 
-val cachedCountWins = Memoized<State,Pair<Long,Long>> { countWins(it) }
+private fun countWinsDp(initial: State): Long {
+    val comparator: Comparator<State> = compareBy { it.score.sum() }
+    val queue = PriorityQueue(comparator)
+    queue.add(initial)
+    val marked = hashSetOf(initial)
 
-private fun countWins(
+    val dp = hashMapOf(initial to 1L).withDefault { 0L }
+    val occurrences = listOf(3 to 1, 4 to 3, 5 to 6, 6 to 7, 7 to 6, 8 to 3, 9 to 1)
+    while (queue.isNotEmpty()) {
+        val state = queue.peek()
+        queue.remove()
+        for ((move, frequency) in occurrences) {
+            val next = state.next(move)
+
+            if (!next.isWinning() && !marked.contains(next)) {
+                queue.add(next)
+                marked.add(next)
+            }
+            dp[next] = dp.getValue(next) + dp.getValue(state) * frequency
+        }
+    }
+
+    return dp.countForResult(21)
+}
+
+private fun State.next(move: Int): State {
+    val newPos = (pos[roll % 2] + move) % 10
+    val newScore = score[roll % 2] + newPos + 1
+    return copy(
+        pos = pos.mapIndexed { i, v -> if (i == (roll % 2)) newPos else v },
+        score = score.mapIndexed { i, v -> if (i == (roll % 2)) newScore else v },
+        roll = roll + 1
+    )
+}
+
+private fun State.isWinning(): Boolean = score.any { it >= 21 }
+
+private fun MutableMap<State, Long>.countForResult(i: Int): Long {
+    val fstResult =
+        this.filterKeys { it.score[0] >= i && it.score[1] < i && it.roll % 2 == 1 }.values.sum()
+    val sndResult =
+        this.filterKeys { it.score[1] >= i && it.score[0] < i && it.roll % 2 == 0 }.values.sum()
+    return max(fstResult, sndResult)
+}
+
+
+val cachedCountWins = Memoized<State, Pair<Long, Long>> { countWinsCached(it) }
+
+private fun countWinsCached(
     state: State,
 ): Pair<Long, Long> {
     if (state.score[0] >= 21) {
@@ -60,19 +116,10 @@ private fun countWins(
         return 0L to 1L
     }
 
-    val current = state.roll % 2
     val occurrences = listOf(3 to 1, 4 to 3, 5 to 6, 6 to 7, 7 to 6, 8 to 3, 9 to 1)
     var result = 0L to 0L
-    for ((pointSum, frequency) in occurrences) {
-        val newPos = (state.pos[current] + pointSum) % 10
-        val newScore = state.score[current] + newPos + 1
-        result += cachedCountWins(
-                state.copy(
-                    pos = state.pos.mapIndexed { i, v -> if (i == current) newPos else v },
-                    score = state.score.mapIndexed { i, v -> if (i == current) newScore else v },
-                    roll = state.roll + 1
-                )
-        ) * frequency.toLong()
+    for ((move, frequency) in occurrences) {
+        result += cachedCountWins(state.next(move)) * frequency.toLong()
     }
     return result
 
